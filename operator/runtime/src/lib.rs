ad7e8addb5215e34d6a3dd29c734a5c11b7033c0
Steve Degosserie <723552+stiiifff@users.noreply.github.com>
2025-04-01 16:13:24 +0200

    Add Preimage pallet

--- operator/runtime/src/lib.rs
+++ operator/runtime/src/lib.rs
@@ -97,6 +97,9 @@ pub const DAYS: BlockNumber = HOURS * 24;
 
 pub const BLOCK_HASH_COUNT: BlockNumber = 2400;
 
+// Provide a common factor between runtimes based on a supply of 10_000_000 tokens.
+pub const SUPPLY_FACTOR: Balance = 1;
+
 // Unit = the base number of indivisible units for balances
 pub const UNIT: Balance = 1_000_000_000_000;
 pub const MILLI_UNIT: Balance = 1_000_000_000;
@@ -104,6 +107,8 @@ pub const MICRO_UNIT: Balance = 1_000_000;
 
 /// Existential deposit.
 pub const EXISTENTIAL_DEPOSIT: Balance = MILLI_UNIT;
+
+pub const STORAGE_BYTE_FEE: Balance = 100 * MICRO_UNIT * SUPPLY_FACTOR;
 
 /// The version information used to identify this runtime when compiled natively.
 #[cfg(feature = "std")]
@@ -246,6 +251,9 @@ mod runtime {
 
     #[runtime::pallet_index(13)]
     pub type EthereumBeaconClient = snowbridge_pallet_ethereum_client;
+
+    #[runtime::pallet_index(22)]
+    pub type Preimage = pallet_preimage;
 
     #[runtime::pallet_index(31)]
     pub type Ethereum = pallet_ethereum;

92b9507bc952b9250366b98d49797e5b1ab3d145
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2025-04-15 13:15:00 +0300

    fix(operator): Update UncheckedExtrinsic creation to use new_bare method

--- operator/runtime/src/apis.rs
+++ operator/runtime/src/apis.rs
@@ -140,7 +140,7 @@ impl fp_self_contained::SelfContainedCall for RuntimeCall {
 
 impl fp_rpc::ConvertTransaction<UncheckedExtrinsic> for TransactionConverter {
     fn convert_transaction(&self, transaction: pallet_ethereum::Transaction) -> UncheckedExtrinsic {
-        UncheckedExtrinsic::new_unsigned(
+        UncheckedExtrinsic::new_bare(
             pallet_ethereum::Call::<Runtime>::transact { transaction }.into(),
         )
     }
@@ -779,7 +779,7 @@ impl_runtime_apis! {
 
     impl fp_rpc::ConvertTransactionRuntimeApi<Block> for Runtime {
         fn convert_transaction(transaction: EthereumTransaction) -> <Block as BlockT>::Extrinsic {
-            UncheckedExtrinsic::new_unsigned(
+            UncheckedExtrinsic::new_bare(
                 pallet_ethereum::Call::<Runtime>::transact { transaction }.into(),
             )
         }

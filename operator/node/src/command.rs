94d6722708732aca07ce4c634890a2d2ba750a04
Steve Degosserie <723552+stiiifff@users.noreply.github.com>
2025-04-17 11:06:55 +0200

    cargo fmt

--- operator/node/src/command.rs
+++ operator/node/src/command.rs
@@ -219,7 +219,7 @@ pub fn run() -> sc_cli::Result<()> {
                                 inherent_benchmark_data()?,
                                 Vec::new(),
                                 &ext_builder,
-                                false
+                                false,
                             )
                         })
                     }

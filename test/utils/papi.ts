8c8210087e59d88a3737c044d14a7f3001ccff17
timbrinded <79199034+timbrinded@users.noreply.github.com>
2025-05-14 09:36:19 +0100

    add some tests

--- test/utils/papi.ts
+++ test/utils/papi.ts
@@ -1,6 +1,19 @@
 import { secp256k1 } from "@noble/curves/secp256k1";
 import { keccak_256 } from "@noble/hashes/sha3";
+import { datahaven } from "@polkadot-api/descriptors";
+import {
+  type HexString,
+  type PolkadotClient,
+  type Transaction,
+  type TxEvent,
+  type TypedApi,
+  createClient
+} from "polkadot-api";
+import { withPolkadotSdkCompat } from "polkadot-api/polkadot-sdk-compat";
 import { type PolkadotSigner, getPolkadotSigner } from "polkadot-api/signer";
+import { getWsProvider } from "polkadot-api/ws-provider/web";
+import { SUBSTRATE_FUNDED_ACCOUNTS } from "./constants";
+import { logger } from "./logger";
 
 // A signer for EVM like chains that use AccountId20 as their public address
 export const getEvmEcdsaSigner = (privateKey: string): PolkadotSigner => {
@@ -30,4 +43,36 @@ export const signEcdsa = (
   result[signedBytes.length] = signature.recovery;
 
   return result;
+};
+
+export const createPapiConnectors = (
+  wsUrl?: string
+): { client: PolkadotClient; typedApi: DatahavenApi } => {
+  const url = wsUrl ?? "ws://127.0.0.1:9944";
+  const client = createClient(withPolkadotSdkCompat(getWsProvider(url)));
+  return { client, typedApi: client.getTypedApi(datahaven) };
+};
+
+export const getPapiSigner = (person: keyof typeof SUBSTRATE_FUNDED_ACCOUNTS = "ALITH") =>
+  getEvmEcdsaSigner(SUBSTRATE_FUNDED_ACCOUNTS[person].privateKey);
+
+export type DatahavenApi = TypedApi<typeof datahaven>;
+
+export const sendTxn = async <P extends {}, R extends string, Q extends string, T>(
+  ext: Transaction<P, R, Q, T>,
+  signer?: PolkadotSigner
+): Promise<TxEvent> => {
+  const papiSigner = signer ?? getPapiSigner();
+  return new Promise((resolve, reject) => {
+    ext.signSubmitAndWatch(papiSigner).subscribe({
+      next: (event) => {
+        logger.debug(`Txn ${event.txHash} event: ${event.type}`);
+        if (event.type === "txBestBlocksState") {
+          console.log("The tx is now in a best block, check it out:");
+          resolve(event);
+        }
+      },
+      error: reject
+    });
+  });
 };

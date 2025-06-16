f2d9e1ec655e75161b9b8934e988d509475624fe
timbrinded <79199034+timbrinded@users.noreply.github.com>
2025-06-16 08:35:13 +0100

    enable json formatting

--- test/scripts/deploy-contracts.ts
+++ test/scripts/deploy-contracts.ts
@@ -31,6 +31,8 @@ export const buildContracts = async () => {
     stderr: buildStderr,
     stdout: buildStdout
   } = await $`forge build`.cwd("../contracts").nothrow().quiet();
+  
+  await $`bun run biome format --write ../contracts/deployments/state-diff.json`;
 
   if (buildExitCode !== 0) {
     logger.error(buildStderr.toString());

18590ceb76df456b5356aa6d5aa42ce4e6b431f1
Gonza Montiel <gon.montiel@gmail.com>
2025-07-07 13:33:12 +0200

    refactor: use utilitary function in MessageEncoding test

--- contracts/test/MessageEncoding.t.sol
+++ contracts/test/MessageEncoding.t.sol
@@ -4,15 +4,15 @@ pragma solidity ^0.8.27;
 import {Test} from "forge-std/Test.sol";
 import {console} from "forge-std/console.sol";
 import {DataHavenSnowbridgeMessages} from "../src/libraries/DataHavenSnowbridgeMessages.sol";
+import {TestUtils} from "./utils/TestUtils.sol";
 
 // This test is used to encode the receive validators message and print the hex string.
 // Run forge test --match-test testEncodeReceiveValidatorsMessage -vvv to see the hex encoded bytes.
 // Use the helper script in operator/test/scripts/test_message_encoding.sh to test the encoding/decoding full cycle.
 contract MessageEncodingTest is Test {
     function testEncodeReceiveValidatorsMessage() public pure {
-        bytes32[] memory mockValidators = new bytes32[](2);
-        mockValidators[0] = bytes32(uint256(1));
-        mockValidators[1] = bytes32(uint256(2));
+        // Use the utility function for consistency
+        address[] memory mockValidators = TestUtils.generateMockValidators(3);
 
         DataHavenSnowbridgeMessages.NewValidatorSetPayload memory payload =
             DataHavenSnowbridgeMessages.NewValidatorSetPayload({validators: mockValidators});

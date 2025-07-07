a2de6bec1c08c668a852319cdd8a9050c11aea5e
Gonza Montiel <gon.montiel@gmail.com>
2025-07-07 14:34:19 +0200

    fix: update wagmi

--- test/contract-bindings/generated.ts
+++ test/contract-bindings/generated.ts
@@ -2615,7 +2615,7 @@ export const dataHavenServiceManagerAbi = [
   {
     type: 'function',
     inputs: [
-      { name: 'solochainAddress', internalType: 'bytes32', type: 'bytes32' },
+      { name: 'solochainAddress', internalType: 'address', type: 'address' },
     ],
     name: 'updateSolochainAddressForValidator',
     outputs: [],
@@ -2625,7 +2625,7 @@ export const dataHavenServiceManagerAbi = [
     type: 'function',
     inputs: [{ name: '', internalType: 'address', type: 'address' }],
     name: 'validatorEthAddressToSolochainAddress',
-    outputs: [{ name: '', internalType: 'bytes32', type: 'bytes32' }],
+    outputs: [{ name: '', internalType: 'address', type: 'address' }],
     stateMutability: 'view',
   },
   {

7ea2f07e013eb7a9f582693a32b87e4a9f6007fa
undercover-cactus <lola@moonsonglabs.com>
2025-07-24 16:17:26 +0200

    remove duplicate definition of

---
+++ contracts/script/utils/ValidatorsUtils.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+import {MerkleUtils} from "../../src/libraries/MerkleUtils.sol";
+import {BeefyClient} from "snowbridge/src/BeefyClient.sol";
+
+library ValidatorsUtils {
+
+    function _buildValidatorSet(
+        uint128 id,
+        bytes32[] memory validators
+    ) internal pure returns (BeefyClient.ValidatorSet memory) {
+        // Calculate the merkle root from the validators array using the shared library
+        bytes32 merkleRoot = MerkleUtils.calculateMerkleRootUnsorted(validators);
+
+        // Create and return the validator set with the calculated merkle root
+        return
+            BeefyClient.ValidatorSet({id: id, length: uint128(validators.length), root: merkleRoot});
+    }
+
+}

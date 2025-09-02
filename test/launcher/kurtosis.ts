1c08202c44463b552018bd97d58c4844b2914684
Steve Degosserie <723552+stiiifff@users.noreply.github.com>
2025-09-02 09:42:15 +0200

    fix: More logs

--- test/launcher/kurtosis.ts
+++ test/launcher/kurtosis.ts
@@ -311,7 +311,7 @@ export const runKurtosisEnclave = async (
   logger.info(`⚙️ Using Kurtosis config file: ${configFile}`);
 
   await runShellCommandWithLogger(
-    `kurtosis run github.com/ethpandaops/ethereum-package --args-file ${configFile} --enclave ${options.kurtosisEnclaveName}`,
+    `kurtosis run github.com/ethpandaops/ethereum-package --args-file ${configFile} --enclave ${options.kurtosisEnclaveName} --cli-log-level debug`,
     {
       logLevel: "debug"
     }

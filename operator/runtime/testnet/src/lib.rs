788e5efaa449a2125e3901c1630213f732c87399
Ahmad Kaouk <56095276+ahmadkaouk@users.noreply.github.com>
2025-09-12 09:45:26 +0200

    feat: Add proxy Precompile (#155)

--- operator/runtime/testnet/src/lib.rs
+++ operator/runtime/testnet/src/lib.rs
@@ -1371,8 +1371,9 @@ macro_rules! get {
 
 #[cfg(test)]
 mod tests {
+    use crate::configs::ProxyType;
     use codec::Decode;
-    use datahaven_runtime_common::{gas::BLOCK_STORAGE_LIMIT, proxy::ProxyType};
+    use datahaven_runtime_common::gas::BLOCK_STORAGE_LIMIT;
 
     use super::{
         configs::{BlockGasLimit, WeightPerGas},

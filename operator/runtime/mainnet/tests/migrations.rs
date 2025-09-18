2fb0dd55f2b9228134417c010539aea7e84a266b
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2025-09-18 23:10:51 +0200

    test: mainnet: enforce pallet migrations root gate

--- operator/runtime/mainnet/tests/migrations.rs
+++ operator/runtime/mainnet/tests/migrations.rs
@@ -1,14 +1,23 @@
-use crate::common::*;
-use datahaven_mainnet_runtime::{pallet_migrations, RuntimeCall, RuntimeOrigin};
+#[path = "common.rs"]
+mod common;
+
+use common::*;
+use datahaven_mainnet_runtime::{Runtime, RuntimeCall, RuntimeOrigin};
 use frame_support::{assert_noop, assert_ok};
-use sp_runtime::DispatchError;
+use pallet_migrations::Call as MigrationsCall;
+use sp_runtime::{traits::Dispatchable, DispatchError};
 
 #[test]
 fn migrations_force_calls_are_root_only() {
     ExtBuilder::default().build().execute_with(|| {
-        let call = RuntimeCall::MultiBlockMigrations(pallet_migrations::Call::force_onboard_mbms {});
+        let call =
+            RuntimeCall::MultiBlockMigrations(MigrationsCall::<Runtime>::force_onboard_mbms {});
 
-        assert_noop!(call.clone().dispatch(RuntimeOrigin::signed(account_id(ALICE))), DispatchError::BadOrigin);
+        assert_noop!(
+            call.clone()
+                .dispatch(RuntimeOrigin::signed(account_id(ALICE))),
+            DispatchError::BadOrigin
+        );
         assert_ok!(call.dispatch(RuntimeOrigin::root()));
     });
 }

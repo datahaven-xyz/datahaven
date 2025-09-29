a537accd99865f006cb06cd5e11d4b5983b991eb
Steve Degosserie <723552+stiiifff@users.noreply.github.com>
2025-09-29 22:39:44 +0200

    feat: ✨ Bump client version to v0.2.0 & runtime spec_version to 200

--- operator/runtime/mainnet/src/lib.rs
+++ operator/runtime/mainnet/src/lib.rs
@@ -120,9 +120,9 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // The version of the runtime specification. A full node will not attempt to use its native
     //   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
     //   `spec_version`, and `authoring_version` are the same between Wasm and native.
-    // This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
+    // This value is set to 200 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
     //   the compatible custom types.
-    spec_version: 100,
+    spec_version: 200,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,

442dd61d1c83f4b45c12167cbafb81701bd97fba
Facundo Farall <37149322+ffarall@users.noreply.github.com>
2025-11-03 01:11:07 -0300

    feat: :sparkles: Upgrade runtimes to SH v0.1.0

--- operator/runtime/stagenet/src/configs/storagehub/mod.rs
+++ operator/runtime/stagenet/src/configs/storagehub/mod.rs
@@ -37,7 +37,7 @@ use sp_runtime::traits::Zero;
 use sp_runtime::SaturatedConversion;
 use sp_runtime::{traits::BlakeTwo256, Perbill};
 use sp_std::convert::{From, Into};
-use sp_std::vec;
+use sp_std::{vec, vec::Vec};
 use sp_trie::{LayoutV1, TrieConfiguration, TrieLayout};
 
 #[cfg(feature = "std")]
@@ -450,6 +450,25 @@ parameter_types! {
     pub const FileSystemFileDeletionRequestHoldReason: RuntimeHoldReason = RuntimeHoldReason::FileSystem(pallet_file_system::HoldReason::FileDeletionRequestHold);
 }
 
+// Converts a given signed message in a EIP-191 compliant message bytes to verify.
+/// EIP-191: https://eips.ethereum.org/EIPS/eip-191
+/// "\x19Ethereum Signed Message:\n" + len(message) + message"
+pub struct Eip191Adapter;
+impl shp_traits::MessageAdapter for Eip191Adapter {
+    fn bytes_to_verify(message: &[u8]) -> Vec<u8> {
+        const PREFIX: &str = "\x19Ethereum Signed Message:\n";
+        let len = message.len();
+        let mut len_string_buffer = itoa::Buffer::new();
+        let len_string = len_string_buffer.format(len);
+
+        let mut eth_message = Vec::with_capacity(PREFIX.len() + len_string.len() + len);
+        eth_message.extend_from_slice(PREFIX.as_bytes());
+        eth_message.extend_from_slice(len_string.as_bytes());
+        eth_message.extend_from_slice(message);
+        eth_message
+    }
+}
+
 impl pallet_file_system::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
     type WeightInfo = pallet_file_system::weights::SubstrateWeight<Runtime>;
@@ -501,6 +520,8 @@ impl pallet_file_system::Config for Runtime {
     type TickRangeToMaximumThreshold = runtime_config::TickRangeToMaximumThreshold;
     type OffchainSignature = Signature;
     type OffchainPublicKey = <Signature as Verify>::Signer;
+    type MaxFileDeletionsPerExtrinsic = ConstU32<100>;
+    type IntentionMsgAdapter = Eip191Adapter;
 }
 
 impl MostlyStablePriceIndexUpdaterConfig for Runtime {

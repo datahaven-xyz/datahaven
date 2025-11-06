3b5896b4f3018afc1c6675c35139b1cc68764b29
Steve Degosserie <723552+stiiifff@users.noreply.github.com>
2025-11-06 18:26:07 +0200

    Revert "feat: add Slashing mode has a runtime configurable parameter (#272)"
    This reverts commit e248a48385220f742892b8cb367503b173208562.

--- operator/pallets/external-validator-slashes/src/lib.rs
+++ operator/pallets/external-validator-slashes/src/lib.rs
@@ -159,10 +159,6 @@ pub mod pallet {
 
         /// The weight information of this pallet.
         type WeightInfo: WeightInfo;
-
-        /// Slashing mode option configuration
-        #[pallet::constant]
-        type SlashingMode: Get<SlashingModeOption>;
     }
 
     #[pallet::error]
@@ -365,16 +361,6 @@ pub mod pallet {
         fn on_initialize(_n: BlockNumberFor<T>) -> Weight {
             let processed = Self::process_slashes_queue(T::QueuedSlashesProcessedPerBlock::get());
             T::WeightInfo::process_slashes_queue(processed)
-        }
-
-        fn on_runtime_upgrade() -> Weight {
-            // Set configured slashing mode when upgrading to runtime RT400
-            if T::RuntimeVersion::get().spec_version < 400 {
-                SlashingMode::<T>::put(T::SlashingMode::get());
-                T::DbWeight::get().writes(1)
-            } else {
-                0
-            }
         }
     }
 }

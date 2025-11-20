71ec99478d6c811af3536c9dd4109cb58be8b4f4
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2025-11-20 10:35:31 +0100

    fix: update expected signature hardcoded values

--- test/datahaven/suites/dev/moonbase/eth-tx/test-eth-tx-types.ts
+++ test/datahaven/suites/dev/moonbase/eth-tx/test-eth-tx-types.ts
@@ -1,14 +1,14 @@
 import { describeSuite, expect } from "@moonwall/cli";
 import { ALITH_ADDRESS, BALTATHAR_ADDRESS, createEthersTransaction } from "@moonwall/util";
 import type { EthereumTransactionTransactionV2 } from "@polkadot/types/lookup";
-import { CHAIN_ID } from "utils";
+import { CHAIN_ID } from "utils/constants";
 import { DEFAULT_TXN_MAX_BASE_FEE } from "../../../../helpers";
 
 describeSuite({
   id: "D021304",
   title: "Ethereum Transaction - Legacy",
   foundationMethods: "dev",
-  testCases: ({ context, it, log }) => {
+  testCases: ({ context, it }) => {
     const DATAHAVEN_CHAIN_ID = CHAIN_ID;
     it({
       id: "T01",
@@ -43,10 +43,10 @@ describeSuite({
         const expectedVBase = DATAHAVEN_CHAIN_ID * 2 + 35;
         expect([expectedVBase, expectedVBase + 1]).to.include(actualV);
         expect(signature.r.toHex()).to.equal(
-          "0xc4d57ab7b0e601a95299b70a46fdbb16371b477669e0f8245e0a9f12e27e15f2"
+          "0xbb5b5f596668edaeeba96caf66b361ca2bbbe8e325e75abd7aee7f8399cb1679"
         );
         expect(signature.s.toHex()).to.equal(
-          "0x4186b0a32dd279fed20b1f20805845e24eff2a2a035801fe19419c09e861a62d"
+          "0x5a010be3c9f198c9e2f6681e0b95a66a741aa1e9ea63cbb2d57f02885d9beefc"
         );
       }
     });
@@ -75,8 +75,19 @@ describeSuite({
 
         expect(extrinsic.isEip2930).to.be.true;
 
-        const { chainId, nonce, gasPrice, gasLimit, action, value, input, accessList, signature } =
-          extrinsic.asEip2930;
+        const {
+          chainId,
+          nonce,
+          gasPrice,
+          gasLimit,
+          action,
+          value,
+          input,
+          accessList,
+          oddYParity,
+          r,
+          s
+        } = extrinsic.asEip2930;
         expect(chainId.toNumber()).to.equal(DATAHAVEN_CHAIN_ID);
         expect(nonce.toNumber()).to.equal(currentNonce);
         expect(gasPrice.toNumber()).to.equal(DEFAULT_TXN_MAX_BASE_FEE);
@@ -85,12 +96,12 @@ describeSuite({
         expect(value.toBigInt()).to.equal(512n);
         expect(input.toHex()).to.equal("0x");
         expect(accessList.toString()).toBe("[]");
-        expect(signature.oddYParity.isTrue).to.be.true;
-        expect(signature.r.toHex()).to.equal(
-          "0x8b978b8a38a3237af932f1988af0b01e60311a440c80bfcae96d7b9ac4ef8310"
+        expect(oddYParity.isFalse).to.be.true;
+        expect(r.toHex()).to.equal(
+          "0x1a703ae78b4f5bd48b04e848a0e261c195e037f39a4e1e2b2637edfe7bdf5328"
         );
-        expect(signature.s.toHex()).to.equal(
-          "0x67c4d6d489d7d5180c8764eb4eff3e16e0330c9a0000b52756847b9ca14069e1"
+        expect(s.toHex()).to.equal(
+          "0x772b2d95acc14739bdd57565a87ce4e51fb7457724e4c42b148c544e4ae3e968"
         );
       }
     });
@@ -102,6 +113,7 @@ describeSuite({
         const currentNonce = await context
           .viem("public")
           .getTransactionCount({ address: ALITH_ADDRESS });
+
         await context.createBlock(
           await createEthersTransaction(context, {
             to: BALTATHAR_ADDRESS,
@@ -129,7 +141,9 @@ describeSuite({
           value,
           input,
           accessList,
-          signature
+          oddYParity,
+          r,
+          s
         } = extrinsic.asEip1559;
         expect(chainId.toNumber()).to.equal(DATAHAVEN_CHAIN_ID);
         expect(nonce.toNumber()).to.equal(currentNonce);
@@ -140,12 +154,12 @@ describeSuite({
         expect(value.toBigInt()).to.equal(512n);
         expect(input.toHex()).to.equal("0x");
         expect(accessList.toString()).toBe("[]");
-        expect(signature.oddYParity.isFalse).to.be.true;
-        expect(signature.r.toHex()).to.equal(
-          "0x6a11d199415bee29b89a65a689546bbb7f50e95fae3a7d238e6a0d5e9753f998"
+        expect(oddYParity.isFalse).to.be.true;
+        expect(r.toHex()).to.equal(
+          "0x07a83a8cea51ecfc21533dbec98de47b37d7f54110395b2b9fd514a9216bb741"
         );
-        expect(signature.s.toHex()).to.equal(
-          "0x45ec58c0626fef976a5e3a9cbad8d3aadbad03a4784fd7ca0cd332d2f571a000"
+        expect(s.toHex()).to.equal(
+          "0x6448665043b9a23baa7d58c3f26c8a291f0db6c38a36a7df21bcc26091f1c5aa"
         );
       }
     });

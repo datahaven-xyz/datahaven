1eb2ef43e3ce6ad3537add038bebb4fe3c1767fb
undercover-cactus <lola@moonsonglabs.com>
2025-12-09 15:08:16 +0100

    added forgotten exit code; check in ci for file timestamp;

--- test/scripts/check-generated-state.js
+++ test/scripts/check-generated-state.js
@@ -5,16 +5,24 @@ const contractsPath = "../contracts/src";
 
 // Get our generated state creation time
 const stateGeneratedCTime = fs.lstatSync("../contracts/deployments/state-diff.json").ctime;
+console.log(stateGeneratedCTime)
 
 // Only read our .sol files
 const contents = fs.readdirSync(contractsPath, { recursive: true });
 
-for (const content of contents) {
-  const stats = fs.lstatSync(path.join(contractsPath, content));
-  if (stats.isFile()) {
-    // if this a .sol file check the date
-    if (stateGeneratedCTime < stats.mtime) {
-      throw "State generated file is outdated. Please regenerate it with `bun ./scripts/generate-contracts.ts`";
+try {
+  for (const content of contents) {
+    const stats = fs.lstatSync(path.join(contractsPath, content));
+    if (stats.isFile()) {
+      console.log(stats.mtime)
+
+      // if this a .sol file check the date
+      if (stateGeneratedCTime < stats.mtime) {
+        throw "State generated file is outdated. Please regenerate it with `bun ./scripts/generate-contracts.ts`";
+      }
     }
   }
+} catch (_err) {
+  // Actually exit the process with a non-zero code
+  process.exit(1);
 }
